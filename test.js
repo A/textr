73920559ad7f7b10d71aadc77c2c245782207508
Shuvalov Anton <anton_shuvalov@me.com>
2015-04-11 20:20:11 +0300

    decompose and immproove the tests

--- test.js
+++ test.js
@@ -2,35 +2,94 @@
 
 'use strict';
 
-var textr = require('./')();
-var assert    = require('assert');
-var quotes    = require('typographic-quotes');
-var ellipses  = require('typographic-ellipses');
-var spaces    = require('typographic-single-spaces');
+var textr  = require('./');
+var should = require('should');
 
-describe('typo', function() {
-  before(function() {
-    textr
-      .use(function(text) { return quotes(text, 'ru'); })
-      .use(ellipses)
-      .use(spaces);
+describe('textr', function() {
+
+  describe('api', function() {
+
+    it('should register transformers', function() {
+      var tf = textr();
+      tf.should.have.property('use');
+      tf.use.should.be.a.Function;
+    });
+
+    it('should parse text', function() {
+      var tf = textr();
+      tf.should.be.a.Function;
+      tf.should.have.property('parse');
+      tf.parse.should.be.a.Function;
+      tf('hello').should.be.equal('hello');
+    });
+
   });
 
-  it('should correct the quotes', function() {
-    var input  = 'hello "world"';
-    var output = 'hello «world»';
-    assert(textr(input) === output);
-  });
+  describe('use', function() {
 
-  it('should correct ellipses', function() {
-    var input  = 'omg...';
-    var output = 'omg…';
-    assert(textr(input) === output);
-  });
+    it('should return transformer', function() {
+      var tf = textr().use(noop);
+      tf.should.be.a.Function;
+      tf.use.should.be.a.Function;
+    });
 
-  it('should remove extra spaces', function() {
-    var input  = 'extra       spaces';
-    var output = 'extra spaces';
-    assert(textr(input) === output);
-  });
+    it('should use plugins', function() {
+      textr()
+        .use(noop, noop)
+        .use(noop)
+        .mws.should.has.length(3)
+      ;
+    });
+
+  })
+
+  describe('parse', function() {
+
+    it('should apply transformers to given text', function() {
+      textr()
+        .use(capitalize)
+        .parse('hello world')
+        .should.be.equal('Hello World')
+      ;
+    });
+
+    it('should apply transformers with order of registration', function() {
+      textr()
+        .use(capitalize)
+        .use(headline(1))
+        .parse('hello world')
+        .should.be.equal('<h1>Hello World</h1>')
+      ;
+    });
+
+  })
+
 });
+
+
+/**
+ * Convert text to capitalized string
+ * @param {String} text
+ * @return {String}
+ */
+function capitalize(text) {
+  return text.replace(/\b(.{1})/g, function (l) {
+    return l.toUpperCase()
+  });
+}
+
+/**
+ * Wrap text by html headline with given level
+ * @param {Number} level
+ * @return {Function}
+ */
+function headline(level) {
+  return function(text) {
+    return '<h' + level + '>' + text + '</h' + level + '>';
+  }
+}
+
+/**
+ * noop function
+ */
+function noop() {}
